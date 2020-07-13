a1c840b4f9626bf6a436d0a901d62f5d4926a82f
Jackson Veroneze <jackson@jacksonveroneze.com>
2020-07-12 22:40:50 -0300

    First commit.

---
+++ gRPCService.CreditRatingClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CreditRatingService;
+using Grpc.Net.Client;
+
+namespace gRPCService.CreditRatingClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = 
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            
+            var channel = GrpcChannel.ForAddress("https://localhost:5001",new GrpcChannelOptions { HttpHandler = httpHandler });
+            
+            var client =  new CreditRatingCheck.CreditRatingCheckClient(channel);
+            var creditRequest = new CreditRequest { CustomerId = "id0201", Credit = 7000};
+            var reply = await client.CheckCreditRequestAsync(creditRequest);
+
+            Console.WriteLine($"Credit for customer {creditRequest.CustomerId} {(reply.IsAccepted ? "approved" : "rejected")}!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            
+        }
+        
+    }
+}
